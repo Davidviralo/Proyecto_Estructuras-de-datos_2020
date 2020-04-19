0f14c24d2e42b1b454709e7414f59891c0e6486a
dviracachas <dviracachas@unal.edu.co>
2020-04-19 17:09:20 -0500

    Clase MateriaPrima
    Se agrega la clase MateriaPrima con sus setters y getters y su constructor. Hasta el momento ninguna clase implementa nuestras propias estructuras de datos.

---
+++ data/MateriaPrima.java
@@ -0,0 +1,62 @@
+package data;
+
+import java.util.ArrayList;
+
+public class MateriaPrima {
+
+    private String admissionDate;
+    private String expirationDate;
+    private String batch;
+    private String description;
+    private ArrayList parametrosCalidad;
+
+    public String getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(String admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList getParametrosCalidad() {
+        return parametrosCalidad;
+    }
+
+    public void setParametrosCalidad(ArrayList parametrosCalidad) {
+        this.parametrosCalidad = parametrosCalidad;
+    }
+
+    public MateriaPrima(String admissionDate, String expirationDate,
+                        String batch, String description, ArrayList parametrosCalidad) {
+
+        this.admissionDate = admissionDate;
+        this.expirationDate = expirationDate;
+        this.batch = batch;
+        this.description = description;
+        this.parametrosCalidad = parametrosCalidad;
+    }
+}
