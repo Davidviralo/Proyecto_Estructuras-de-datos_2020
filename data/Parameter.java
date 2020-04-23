fe424a625e8617787148175ab3e2b66c1441294a
Fabian <fsgalindope@unal.edu.co>
2020-04-23 16:37:05 -0500

    Edicion parameter
    gettters setters y satistyquality

--- data/Parameter.java
+++ data/Parameter.java
@@ -1,4 +1,53 @@
 package data;
 
 public class Parameter {
+    private String name;
+    private double value;
+    private double lowerLimit;
+    private double upperLimit;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getLowerLimit() {
+        return lowerLimit;
+    }
+
+    public double getUpperLimit() {
+        return upperLimit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void setLowerLimit(double lowerLimit) {
+        this.lowerLimit = lowerLimit;
+    }
+
+    public void setUpperLimit(double upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
+    public Parameter(String name, double value, double lowerLimit, double upperLimit) {
+        this.name = name;
+        this.value = value;
+        this.lowerLimit = lowerLimit;
+        this.upperLimit = upperLimit;
+    }
+     
+    public boolean satisfyQuality(){
+    return(lowerLimit<=value)&&(value<=upperLimit);
+    
+    }
+   
 }
