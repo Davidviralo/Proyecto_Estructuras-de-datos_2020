ed54684ca425b0e4c47ad20b0409d1717beabb6f
Fabian <fsgalindope@unal.edu.co>
2020-04-24 17:30:07 -0500

    constructor2 parametwer

--- data/Parameter.java
+++ data/Parameter.java
@@ -1,5 +1,9 @@
 package data;
 
+import Estructuras_de_datos.ArrayQueue;
+import Estructuras_de_datos.MyArrayList;
+import java.util.Scanner;
+
 public class Parameter {
     private String name;
     private double value;
@@ -37,13 +41,38 @@ public class Parameter {
     public void setUpperLimit(double upperLimit) {
         this.upperLimit = upperLimit;
     }
-
+public Parameter(){
+        Scanner input = new Scanner(System.in);
+       /* int numeroetapas= teclado.nextInt();
+        System.out.println("Porfavor ingrese el numero de etapas");
+        for (int i=0;i<numeroetapas;i++){
+         System.out.println("Etapa. nombre,id,starDate,description");
+            String nameStage = teclado.nextLine();
+            int id = teclado.nextInt();
+            String StartDate = teclado.nextLine();
+            String descrption = teclado.nextLine();
+            MyArrayList<Parameter> parameters = new MyArrayList<>();
+            ProcessStage etapa1 = new ProcessStage("", "StartDate", "StartDate", "descrption",parameters); 
+            ArrayQueue<ProcessStage> listaProcesos = new ArrayQueue<>();
+        this();*/
+            System.out.println("Creación de parametro");
+            System.out.println("Digite el valor");
+            this.value= input.nextDouble();
+            System.out.println("Digite limite inferior");
+            this.lowerLimit= input.nextDouble();
+            System.out.println("Digite limite superior");
+            this.upperLimit= input.nextDouble();
+            System.out.println("Digite el nombre");
+            this.name = input.next();
+        
+    }
     public Parameter(String name, double value, double lowerLimit, double upperLimit) {
         this.name = name;
         this.value = value;
         this.lowerLimit = lowerLimit;
         this.upperLimit = upperLimit;
     }
+    
      
     public boolean satisfyQuality(){
     return(lowerLimit<=value)&&(value<=upperLimit);
