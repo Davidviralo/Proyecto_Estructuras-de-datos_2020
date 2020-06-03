c757650f32a4e36672d6d7f84d25b8ed510c54c3
Fabian <fsgalindope@unal.edu.co>
2020-06-03 10:14:54 -0500

    Clase control de los paneles

--- businessLogic/wrapLayout.java
+++ businessLogic/wrapLayout.java
@@ -5,11 +5,9 @@
  */
 package businessLogic;
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Insets;
+import java.awt.*;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 /**
  *
